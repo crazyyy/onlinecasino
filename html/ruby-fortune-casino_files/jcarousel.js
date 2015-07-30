fefdad4a1f73a2e21a72b3f5c93e88c746e477d3
crazyyy <crazyyy@gmail.com>
2015-07-30 23:53:42 +0300

    work with this

--- html/ruby-fortune-casino_files/jcarousel.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".slider-wrapper #jcl").jCarouselLite({
-        btnNext: ".screenshot-slider .lnk-next",
-        btnPrev: ".screenshot-slider .lnk-prev",
-        speed: 500,
-    	visible: 4
-    });     
- });
